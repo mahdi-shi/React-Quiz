806fdb36eb69c7bb9b276da6a3374d65d2a50c4e
Tailus Dev <4932abcd@gmail.com>
2024-02-24 22:06:06 +0330

    json-sever added / creating fake api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import DateCounter from "./DateCounter.js"
-
+import Main from "./Main.js";
+import Header from "./Header.js"
 export default function App() {
   return (
-    <div className="App">
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
-
